a9425e8dd0cba0be7e2974a43990a78bb15a1595
Jeremy Matthew Case <jcase2@ubuntu.statler.wvu.edu>
2016-10-18 18:52:47 -0400

    Added Jcase button

--- NameGUI.java
+++ NameGUI.java
@@ -21,6 +21,7 @@ class NameGUI extends JFrame implements ActionListener{
         JButton btnSample = new JButton("Sample Button");
         JButton btnQuit = new JButton("Quit");
         JButton btnSwap = new JButton("Swap");
+	JButton btnNameJcase = new JButton("Display Name Case");
         
         //create sample textboxes
         txtWord1 = new JTextField(15);
@@ -36,12 +37,15 @@ class NameGUI extends JFrame implements ActionListener{
         btnQuit.addActionListener(this);
         btnSwap.setActionCommand("swap");  
         btnSwap.addActionListener(this);
+        btnNameJcase.setActionCommand("jcase");  
+        btnNameJcase.addActionListener(this);
  
         //Add components to proper panels
         panelTop.add(btnSample);
         panelTop.add(btnQuit);
         
         //panelMiddle.add(btnYourButton);
+	panelMiddle.add(btnNameJcase);
 
         panelBottom.add(lblSwap);
         panelBottom.add(btnSwap);
@@ -121,6 +125,9 @@ class NameGUI extends JFrame implements ActionListener{
                 break;
             case "help" :
                 JOptionPane.showMessageDialog(this,"There is no help for you.","Sorry",JOptionPane.WARNING_MESSAGE);
+                break;
+	    case "jcase":
+                JOptionPane.showMessageDialog(this,"Jeremy Case","This is my name.",JOptionPane.PLAIN_MESSAGE);
                 break;
                 
         }
